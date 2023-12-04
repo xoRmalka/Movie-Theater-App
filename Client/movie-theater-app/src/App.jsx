f021fdf86bb5074827ed04ecf275306e2b020def
Or Malka <ormalka1234@gmail.com>
2023-12-04 10:54:25 +0200

    client: Movie order button

--- Client/movie-theater-app/src/App.jsx
+++ Client/movie-theater-app/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage";
+import MovieOrderPage from "./Pages/HomePage/MovieOrderPage/MovieOrderPage";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/movie_order/:id" element={<MovieOrderPage />} />
       </Routes>
     </div>
   );
--- Client/movie-theater-app/src/Pages/HomePage/Movie/MovieOrderBtn/MovieOrderBtn.jsx
+++ Client/movie-theater-app/src/Pages/HomePage/Movie/MovieOrderBtn/MovieOrderBtn.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function MovieOrderBtn(data) {
-    const { date } = data;
+export default function MovieOrderBtn({ data }) {
+  const { date, schedule_id } = data;
 
-    useEffect(() => {console.log(data);}, []);
+  const navigate = useNavigate();
 
-  return (
-    <button>MovieOrderBtn</button>
-  )
+  const orderMovie = () => {
+    navigate(`/movie_order/${schedule_id}`);
+  };
+
+  const [formattedTime, setFormattedTime] = useState("");
+
+  useEffect(() => {
+    const parsedDate = new Date(date);
+
+    const options = { hour: "2-digit", minute: "2-digit", hour12: false };
+    const formattedTimeString = parsedDate.toLocaleTimeString("en-US", options);
+
+    setFormattedTime(formattedTimeString);
+  }, [date]);
+
+  return <button onClick={orderMovie}>{formattedTime}</button>;
 }
