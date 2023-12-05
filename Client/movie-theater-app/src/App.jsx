26fc67f10a3aad0decc12b6276e020a78a8cdc2c
Or Malka <ormalka1234@gmail.com>
2023-12-05 19:20:46 +0200

    client: Admin page

--- Client/movie-theater-app/src/App.jsx
+++ Client/movie-theater-app/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage/HomePage";
 import MovieOrderPage from "./Pages/MovieOrderPage/MovieOrderPage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import AdminPage from "./Pages/AdminPage/AdminPage";
+import MoviesManagement from "./Pages/AdminPage/MoviesManagement/MoviesManagement";
+import AddMoviePage from "./Pages/AdminPage/MoviesManagement/AddMoviePage/AddMoviePage";
+import EditMoviePage from "./Pages/AdminPage/MoviesManagement/EditMoviePage/EditMoviePage";
 
 function App() {
   return (
@@ -10,6 +14,14 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movie_order/:id" element={<MovieOrderPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/admin" element={<AdminPage />}>
+          <Route path="movies" element={<MoviesManagement />} />
+          <Route path="movies/add" element={<AddMoviePage />} />
+          <Route path="movies/edit/:id" element={<EditMoviePage />} />
+
+        </Route>
       </Routes>
     </div>
   );
