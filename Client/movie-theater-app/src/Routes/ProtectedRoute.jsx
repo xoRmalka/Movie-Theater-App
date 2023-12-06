9077268b3f3bdac00b50b9ba94d385f527b5b00d
Or Malka <ormalka1234@gmail.com>
2023-12-06 16:30:46 +0200

    client: Work on token verification

---
+++ Client/movie-theater-app/src/Routes/ProtectedRoute.jsx
@@ -0,0 +1,57 @@
+// ProtectedRoute.jsx
+import React, { useEffect, useState } from "react";
+import { Navigate, Route } from "react-router-dom";
+import axios from "axios";
+
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const token = localStorage.getItem("adminToken");
+
+        if (!token) {
+          setIsAuthenticated(false);
+          setLoading(false);
+          return;
+        }
+
+        const response = await axios.get(
+          "http://localhost:8001/users/verify/admin",
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+
+        if (response.data.admin) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.error("Error verifying token:", error);
+        setIsAuthenticated(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifyToken();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? (
+    <Route {...rest} element={<Component />} />
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
+
+export default ProtectedRoute;
