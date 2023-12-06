35501d6c5a461fcc1f6c8316f146e128dd9706a1
Or Malka <ormalka1234@gmail.com>
2023-12-06 13:55:28 +0200

    client: Add schedule movie selecttion

---
+++ Client/movie-theater-app/src/Pages/AdminPage/SchedulePage/AddSchedulePage/MovieSelect.jsx/MovieSelect.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function MovieSelect({ schedule, movies, handleMovieChange }) {
+  return (
+    <>
+      <label>
+        Select Movie:{" "}
+        <select value={schedule.movie_id} onChange={handleMovieChange}>
+          <option disabled={schedule.movie_id !== null} value="">
+            Select a Movie
+          </option>
+          {movies.map((movie) => (
+            <option key={movie._id} value={movie._id}>
+              {movie.title}
+            </option>
+          ))}
+        </select>
+      </label>
+      <br />
+      <br />
+    </>
+  );
+}
